f91614232fa111bf668b8edea8a48fa1a6045b58
Kaan Er <erkaan1@outlook.com>
2020-11-05 22:50:02 +0300

    added array method

--- src/Summation.java
+++ src/Summation.java
@@ -1,20 +1,16 @@
-import java.util.Random;
-
 public class Summation extends Thread{
-    private int upper;
-    public static int total;
+    private int[] upper;
+    public static int totalSum;
 
-    public Summation(int upper) {
+    public Summation(int[] upper){
         this.upper = upper;
-        total = 0;
+        totalSum = 0;
     }
 
     @Override
-    public void run() {
-        Random rnd = new Random();
-        for(int i=0;i <upper; i++)
-        {
-            total += rnd.nextInt(10000);
-        }
+    public void run(){
+
+        for(int i = 0; i < upper.length; i++)
+            totalSum += upper[i];
     }
 }
--- src/ThreadExamp.java
+++ src/ThreadExamp.java
@@ -1,23 +1,38 @@
+import java.util.Random;
+
 public class ThreadExamp implements Runnable {
     public static void main(String[] args) {
-        Thread t1 = new Thread(new ThreadExamp());
-        t1.start();
+        Thread thread = new Thread(new ThreadExamp());
+        thread.start();
     }
 
     @Override
     public void run() {
         try {
-            int upper = 1000;
+            int[] rndItem = new int[10000];
+            int[] rndItemTemporary = new int[1000];
+            Random random = new Random();
+
+            for(int i = 0; i < 10000; i++){
+                rndItem[i] = random.nextInt(10000);
+            }
+
+            int k = 0;
             Thread[] threads = new Thread[10];
-            for (int i=0; i<10; i++)
-            {
-                threads[i] = new Summation(upper);
+
+            for(int i = 0; i < threads.length; i++){
+
+                for(int j = k; j < k+1000; j++){
+                    if(j > 999)
+                        rndItemTemporary[j-k] = rndItem[j];
+                }
+                k += 1000;
+                threads[i] = new Summation(rndItemTemporary);
                 threads[i].start();
             }
-            System.out.println("Total : " + Summation.total);
-        }
-        catch (Exception ex){
-            System.out.println(ex.getMessage());
+            System.out.println("Total : " + Summation.totalSum);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
     }
 }
